6105ce692cf98e7ba4aa230948e46a5184022e9f
PGMacDesign <pgmacdesign@gmail.com>
2014-08-06 01:50:22 -0700

    minor update

--- src/com/pgmacdesign/myresume/MainActivity.java
+++ src/com/pgmacdesign/myresume/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pgmacdesign.myresume;
 
+//Main menu/ Activity. Have multiple choice options 
 public class MainActivity {
 
 }
--- src/com/pgmacdesign/myresume/MyApps.java
+++ src/com/pgmacdesign/myresume/MyApps.java
@@ -1,5 +1,6 @@
 package com.pgmacdesign.myresume;
 
+//This class will showcase my apps with links to them and pictures/ descriptions
 public class MyApps {
 
 }
