774b1371558e235f6a2f1be6fe18d7df0492c3f9
PGMacDesign <pgmacdesign@gmail.com>
2014-08-14 10:33:59 -0700

    Added some contact info

---
+++ src/com/pgmacdesign/myresume/ContactInfo.java
@@ -0,0 +1,11 @@
+package com.pgmacdesign.myresume;
+
+//This goes over the various ways to contact me. 
+public class ContactInfo {
+
+	//Include the following:
+	//Address: Courser Ave, La Mirada, CA 90638
+	//Number: (562) 322 - 6971
+	//Email: PGMacDesign@gmail.com
+	
+}
