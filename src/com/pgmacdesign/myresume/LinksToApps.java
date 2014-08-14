43f2572d6b342a90ef13a701b7121f63b2c35354
PGMacDesign <pgmacdesign@gmail.com>
2014-08-13 22:27:08 -0700

    Starting links

---
+++ src/com/pgmacdesign/myresume/LinksToApps.java
@@ -0,0 +1,8 @@
+package com.pgmacdesign.myresume;
+
+//This will link to other applications I have written. Direct web links
+public class LinksToApps {
+
+	//1) RSR Toolbox :: https://play.google.com/store/apps/details?id=com.pgmacdesign.rsrtoolbox
+	//2) Speech to Clipboard :: https://play.google.com/store/apps/details?id=com.pgmacdesign.speechtotextclipboardwidget 
+}
