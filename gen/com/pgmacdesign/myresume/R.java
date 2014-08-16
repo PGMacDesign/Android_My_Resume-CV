cad23becc8f205aa087f12e719b4919a26ba40f1
PGMacDesign <pgmacdesign@gmail.com>
2014-08-15 21:25:04 -0700

    Updating XML files

--- gen/com/pgmacdesign/myresume/R.java
+++ gen/com/pgmacdesign/myresume/R.java
@@ -27,11 +27,11 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f090002;
-        public static final int container=0x7f090000;
-        public static final int textView1=0x7f090001;
+        public static final int listView1=0x7f090001;
+        public static final int textView1=0x7f090000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int main_menu=0x7f030000;
         public static final int splash=0x7f030001;
     }
     public static final class menu {
